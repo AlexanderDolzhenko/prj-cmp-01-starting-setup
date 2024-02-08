5b16c77b04c303608fe1601d5afada86c2595b51
Alexander <dolzhenkoa93@gmail.com>
2024-02-08 18:15:03 +0300

    lklkl

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+export default {
+  components: { StoredResources },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'the official vue documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'learn to google',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
