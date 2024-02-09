f93a068caac3ace35693d58cecd9b5f2ed7db951
Alexander <dolzhenkoa93@gmail.com>
2024-02-09 21:06:44 +0300

    big changes

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -3,6 +3,7 @@
     <learning-resource
       v-for="res in resources"
       :key="res.id"
+      :id="res.id"
       :title="res.title"
       :description="res.description"
       :link="res.link"
@@ -20,7 +21,7 @@ import LearningResource from './LearningResource.vue';
 
 export default {
 components: {LearningResource},
-props: ['resources']
+inject: ['resources']
 }
 </script>
 
